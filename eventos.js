c3d16d9020966e5d3a0ce8ff5a5fb34057d96e4e
Fixner <Fernandochook@gmail.com>
2024-09-08 22:42:57 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const div = document.getElementById('boton');
+    const button = document.querySelector('button'); 
+  
     div.addEventListener('click', function() {
       alert('Hola! Soy el div');
     });
+
+    button.addEventListener('click', function(event) {
+      event.stopPropagation(); 
+      alert('Hola!');
+    });
   });
+  
